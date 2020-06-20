6dd07c7bc366d54ee76be210fd2a0246383818bb
王鼎文 <wongdw@foxmail.com>
2020-06-20 23:54:55 +0800

    提交apoll-service

---
+++ apollo-service/src/main/java/priv/wdw/apollo/ApolloServiceApp.java
@@ -0,0 +1,21 @@
+package priv.wdw.apollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>@author wdw</p>
+ * <p>@date 2020/6/20 20:09</p>
+ * <p>@description nacos-service的启动类 </p>
+ */
+
+@SpringBootApplication
+public class ApolloServiceApp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloServiceApp.class, args);
+    }
+
+
+}
